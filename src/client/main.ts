ae668e71023cc7c2a1088bce8c2d854b1255f774
小莫 <xiaomo1433675239@outlook.com>
2025-01-16 10:25:33 +0800

    修复暗色模式, 为下载添加cdn

--- src/client/main.ts
+++ src/client/main.ts
@@ -1,6 +1,7 @@
+import 'element-plus/theme-chalk/dark/css-vars.css'
+
 
 import { createApp } from "vue";
-
 import App from "@/App.vue"
 import { vuetify } from '@/plugins/vuetify'
 import router from '@/router'
